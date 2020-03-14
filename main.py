961f675b514aa2c59c38c3add2f0df70beb9fc55
yize <yizeliang@126.com>
2020-03-14 20:13:01 +0800

    fix:增加重试间隔为10秒,修复会出现多个npc进程的问题

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def get_ip(domain, dns):
 def npc(domain, vkey, type):
     connect_str = npc_str.format(domain, vkey, type)
     print(connect_str)
-    pi = subprocess.Popen(connect_str, shell=True,
+    pi = subprocess.Popen(connect_str, shell=False,
                           stdout=subprocess.PIPE)
 
     for i in iter(pi.stdout.readline, 'b'):
@@ -34,26 +34,34 @@ def npc(domain, vkey, type):
     pi.kill()
 
 
+sleep_time = 10
+
 if __name__ == '__main__':
     # npc -server=127.0.0.1:7200 -vkey=0ucn6wx7e9zuz4yg -type=tcp
     try:
-        domain = sys.argv[1]
-        port = sys.argv[2]
-        vkey = sys.argv[3]
-        type = sys.argv[4]
-        dns = sys.argv[5]
+        # domain = sys.argv[1]
+        # port = sys.argv[2]
+        # vkey = sys.argv[3]
+        # type = sys.argv[4]
+        # dns = sys.argv[5]
+
+        domain = 'www.yizems.cn'
+        port = '6000'
+        vkey = 'aaa'
+        type = 'tcp'
+        dns = 'f1g1ns1.dnspod.net'
         while True:
             ip = get_ip(domain, dns)
             if ip is None:
                 print("域名无法解析:5秒后开始下次执行")
-                time.sleep(5)
+                time.sleep(sleep_time)
                 continue
 
             print(ip)
 
             npc(ip + ":" + port, vkey, type)
             print("进程结束:5秒后开始下次执行")
-            time.sleep(5)
+            time.sleep(sleep_time)
     except Exception as e:
         print('发生错误,请检查命令')
         print(e)
