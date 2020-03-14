079d929fa0ef6695c811286697918955831343b3
yize <yizeliang@126.com>
2020-03-14 12:15:03 +0800

    第一版

---
+++ README.md
@@ -0,0 +1,44 @@
+# NPC 动态ip解决方案
+
+使用前提,需要有一个域名,和知道域名 生效最快的DNS服务器,一般买域名会给2个DNS服务器地址
+
+**只编译了windows 客户端**
+
+## 思路
+
+调用`nslookup`查询域名地址, 查不到继续等待
+
+查到了 执行npc连接,如果连接出错,则 kill npc进程
+
+等待5秒,再查询域名地址,如此循环
+
+## 打包
+
+`pip install PyInstaller`
+
+`PyInstaller -F main.py`
+
+
+## 使用
+
+### 1 源码编译
+
+### 2 通过命令
+
+`main.py domain port vkey type dns`
+
+## npc
+
+脚本只是负责调用命令,所以,和`npc` 放在同一目录即可
+
+
+## NPS 官方网址
+
+[github](https://github.com/ehang-io/nps)
+
+
+## 备注
+
+本人python贼烂,能用就好=.=,请不要吐糟代码=.=
+
+我没有上传 虚拟环境 ,请自己创建
---
+++ main.py
@@ -0,0 +1,59 @@
+import subprocess
+import sys
+import time
+
+npc_str = "npc.exe -server={} -vkey={} -type={}"
+
+
+def get_ip(domain, dns):
+    res = subprocess.Popen("nslookup {} {}".format(domain, dns), stdin=subprocess.PIPE,
+                           stdout=subprocess.PIPE).communicate()[0]
+    response = res.decode("GBK")
+    res_list = response.split("\r\n")
+    ip = str.strip(res_list[len(res_list) - 3])
+    if ip.startswith("名称"):
+        return None
+    else:
+        return str.strip(ip.replace("Address:", ""))
+
+
+def npc(domain, vkey, type):
+    connect_str = npc_str.format(domain, vkey, type)
+    print(connect_str)
+    pi = subprocess.Popen(connect_str, shell=True,
+                          stdout=subprocess.PIPE)
+
+    for i in iter(pi.stdout.readline, 'b'):
+        out = i.decode("GBK")
+        print(out)
+        if "Reconnecting" in out or "reconnected in five seconds" in out:
+            break
+
+    pi.stdout.close()
+    pi.terminate()
+    pi.kill()
+
+
+if __name__ == '__main__':
+    # npc -server=127.0.0.1:7200 -vkey=0ucn6wx7e9zuz4yg -type=tcp
+    try:
+        domain = sys.argv[1]
+        port = sys.argv[2]
+        vkey = sys.argv[3]
+        type = sys.argv[4]
+        dns = sys.argv[5]
+        while True:
+            ip = get_ip(domain, dns)
+            if ip is None:
+                print("域名无法解析")
+                continue
+
+            print(ip)
+
+            npc(ip + ":" + port, vkey, type)
+            print("进程结束:5秒后开始下次执行")
+            time.sleep(5)
+    except Exception as e:
+        print('发生错误,请检查命令')
+        print(e)
+        print('main domain port vkey type dns')
